0242b03f015aa42f267d9cd39c4ee112c5762c20
Navneet719 <kumar.navneet719@gmail.com>
2022-08-08 22:17:29 +0530

    api

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,29 +10,63 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+// router.get('/students', function (req, res){
+//     let students = ['Sabiha', 'Neha', 'Akash']
+//     res.send(students)
+// })
+
+router.get('/movies', function (req, res) {
+    let movie = ['don', 'soley', 'bahubali']
+    res.send(movie)
 })
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+router.get('/film', function (req, res) {
+    let films = [
+        {
+            'id': 1,
+            'name': 'the shinig'
+        },
+        {
+            'id': '2',
+            'name': 'Incendies'
+        },
+        {
+            'ia': '3',
+            'name': 'rang de basanti'
+        },
+        {
+            'id': '4',
+            'name': 'finding nemo'
 
-    let requestParams = req.params
+        }
+    ]
+    res.send(films)
 
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
+
 })
+
+
+
+
+
+
+/*
+params is an attribute inside request that contains 
+dynamic values.
+This value comes from the request url in the form of an 
+object where key is the variable defined in code 
+and value is what is sent in the request
+*/
+
+// let requestParams = req.params
+
+// JSON strigify function helps to print an entire object
+// We can use any ways to print an object in Javascript, JSON stringify is one of them
+// console.log("This is the request "+ JSON.stringify(requestParams))
+// let studentName = requestParams.name
+// console.log('Name of the student is ', studentName)
+
+// res.send('Dummy response')
+
 
 module.exports = router;
