a09414da583707a5c1423d318a596f5c8a37737d
Navneet719 <kumar.navneet719@gmail.com>
2022-08-15 14:35:39 +0530

    mongo-0

--- src/index.js
+++ src/index.js
@@ -9,19 +9,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
-, {
-   useNewUrlParser: true 
-}
-).then( () => {console.log( "MongoDb is connected")}  )
-.catch( err => console.log(err))
+mongoose.connect("mongodb+srv://navneet:Navneet719@cluster0.3oclrwu.mongodb.net/navneet?retryWrites=true&w=majority", { useNewUrlParser: true })
+
+
+    .then(() => console.log("Mongoose is connected"))
+    .catch(err => console.log(err))
 
 
 
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
 
