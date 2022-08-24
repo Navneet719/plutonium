48a9485287af8dbc35527dd9954726e8b16649a3
Navneet719 <kumar.navneet719@gmail.com>
2022-08-24 18:55:50 +0530

    assignment solution

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require('moment') // time npm
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,8 +18,18 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
+        const today = moment();
+        console.log(today.format())
+
+        // find ip 
+       console.log(req.socket.remoteAddress);
+
+        //path 
+       const url = req.originalUrl
+       console.log(url)
+         
+
+    next();
   }
   );
 
